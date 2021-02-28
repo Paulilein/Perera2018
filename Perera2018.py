af702ed88efa515803e3f9003f7a170d2667fb91
Pauline Tenblad <p.tenblad@uva.nl>
2021-02-28 10:45:01 +0100

    Add .gitignore, update form of Data

---
+++ Perera2018.py
@@ -0,0 +1,27 @@
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Halperin2015_SI_Data.csv', sep=';')
+X = dataset.iloc[:, 1:].values
+y = dataset.iloc[:, 0].values
+# Want y to be in a 2D array [[]]. right now, its a vector
+y = y.reshape(len(y),1)
+
+# Encoding categorical data
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0,3])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+y_train = sc.fit_transform(y_train)
+y_test = sc.transform(y_test)
